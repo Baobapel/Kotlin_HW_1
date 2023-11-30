8085a3ed33dc7b9dd402566b9d20fc0df0790384
Kirill <bra0610@yandex.ru>
2023-12-01 00:16:01 +0300

    wrote code for Transfers.kt

--- src/main/kotlin/Transfers.kt
+++ src/main/kotlin/Transfers.kt
@@ -1,3 +1,10 @@
-fun main() {
+import kotlin.math.roundToInt
 
+fun main() {
+    val amount = 10254
+    val сommissionСoefficient = 0.0075
+    val minCommission = 35
+    val commission = amount * сommissionСoefficient
+    val finalCommission = if (commission >= minCommission) commission.roundToInt() else minCommission
+    println("Вы перевели $amount рублей и заплатили комиссию $finalCommission рублей.")
 }
