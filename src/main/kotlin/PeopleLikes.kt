c1ed26588aa3d1aa43a866f65945602478fefc70
Kirill <bra0610@yandex.ru>
2023-12-03 00:46:22 +0300

    wrote code for PeopleLikes.kt

---
+++ src/main/kotlin/PeopleLikes.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val likes = 21
+    val lastDigit = likes % 10
+    when {
+        lastDigit == 1 && likes != 11 -> println("Понравилось $likes человеку")
+        likes == 11 -> println("Понравилось $likes людям")
+        else -> println("Понравилось $likes людям")
+    }
+}
