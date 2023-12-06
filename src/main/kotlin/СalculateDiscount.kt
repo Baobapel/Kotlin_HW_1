5b77544dde8da285e0e2f445f1ab8d70e0cfb759
Kirill <bra0610@yandex.ru>
2023-12-07 01:27:31 +0300

    refactor code for CalculateDiscount.kt

--- src/main/kotlin/СalculateDiscount.kt
+++ src/main/kotlin/СalculateDiscount.kt
@@ -19,19 +19,16 @@ fun main() {
         else -> {
             purchaseAmount
         }
-
     }
-
     val amountAfterDiscount = purchaseAmount - discount * onepercent
 
     println("Покупка - $purchaseAmount рублей")
-
     when {
         discount == 0.00 -> println("Скидок нет!")
-        purchaseAmount in 1000.0 ..10000.0 -> println("Скидка составила 100 рублей. Сумма составила $simpleDiscount")
+        purchaseAmount in 1000.0..10000.0 -> println("Скидка составила 100 рублей. Сумма составила $simpleDiscount")
         purchaseAmount >= 10001.0 -> println("После применения 5% скидки — скидка составила $discount рублей.")
-        isRegularCustomer -> println("Вы постоянный клиент. После применения 1% скидки — $amountAfterDiscount рублей.")
     }
+    if (isRegularCustomer) println("Вы постоянный клиент. После применения 1% скидки — $amountAfterDiscount рублей.")
 }
 
 
