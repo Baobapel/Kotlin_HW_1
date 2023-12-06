e593a64a122768ebb2fcaba027869a8c51bec1cf
Kirill <bra0610@yandex.ru>
2023-12-06 04:19:09 +0300

    refactor code for PeopleLikes.kt

--- src/main/kotlin/PeopleLikes.kt
+++ src/main/kotlin/PeopleLikes.kt
@@ -1,10 +1,9 @@
-
 fun main() {
-    val likes = 21
-    val lastDigit = likes % 10
-    when {
-        lastDigit == 1 && likes != 11 -> println("Понравилось $likes человеку")
-        likes == 11 -> println("Понравилось $likes людям")
-        else -> println("Понравилось $likes людям")
+    val likes = 154615611
+    val lastWord = when {
+        likes == 1 || (likes % 10 == 1 && likes % 100 != 11) -> "человеку"
+        else -> "людям"
     }
+
+    println("Понравилось $likes $lastWord")
 }
